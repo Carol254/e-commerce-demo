30da514d3f6c763729573fa4d303f5f2b66ddf03
Carol254 <wanguic49@gmail.com>
2023-01-15 19:11:41 +0300

    few components added products && header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',component:ProductListComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
